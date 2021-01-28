4f3780772420a1d8c830e59fe690f57108237a09
Todd Spainhour <toddspainhour@gmail.com>
2021-01-27 19:37:27 -0600

    added SetUpGame method

--- Chapter-01/MatchGame/MatchGame/MainWindow.xaml.cs
+++ Chapter-01/MatchGame/MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,33 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "😊", "😊",
+                "😂", "😂",
+                "😜", "😜",
+                "👍", "👍",
+                "🎂", "🎂",
+                "😃", "😃",
+                "😢", "😢",
+                "😊", "😊",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
